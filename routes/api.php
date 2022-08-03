f6b3b379e9fd2cc2f1dd83065fd992f144895f63
aziizpranaja <pranaja200102@gmail.com>
2022-08-03 19:17:00 +0700

    kelima commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\auth\AuthController;
 use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\API\QrcodeController;
 use App\Http\Controllers\API\AddqueueController;
